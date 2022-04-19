4ea150a39b09b995444bc244c63289fa1fee00be
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-19 19:19:06 -0300

    --

--- geekShopping/GeekShopping/GeekShopping.Web/Program.cs
+++ geekShopping/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,9 +1,15 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Injeção do HTTP Client
+builder.Services.AddHttpClient<IProductService, ProductService>( c => c.BaseAddress=new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
