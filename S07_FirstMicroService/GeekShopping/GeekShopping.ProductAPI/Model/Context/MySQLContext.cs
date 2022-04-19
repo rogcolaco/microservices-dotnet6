704047f26ede329e63d90372180543ceb2de8fa2
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-19 16:44:13 -0300

    Finalizacao conexao banco de dados e criacao da primeira tabela 'Product'

--- S07_FirstMicroService/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ S07_FirstMicroService/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -9,5 +9,7 @@ namespace GeekShopping.ProductAPI.Model.Context {
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){
         }
 
+        public DbSet<Product> Products { get; set; }
+
     }
 }
