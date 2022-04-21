c6e04842ddcaf38408abed3cc0fb644d75abb2de
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-21 16:24:23 -0300

    Configuracao authentication no swagger

--- geekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ geekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -4,6 +4,7 @@ using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 //Configuração conexão banco de dados
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.
-    UseMySql(connection, 
+    UseMySql(connection,
         new MySqlServerVersion(new Version(8, 0, 28))));
 
 //Mapeamento de configuração entre product VO <-> product
@@ -48,6 +49,33 @@ builder.Services.AddAuthorization(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    }); 
+});
 
 var app = builder.Build();
 
