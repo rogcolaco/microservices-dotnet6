1edef065bc8ec6bb201c32afffe8c369b4de5505
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-21 16:41:05 -0300

    Inicio da etapa de desenvolvimento das telas da aplicacao para login

--- geekShopping/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ geekShopping/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShopping.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -21,6 +22,15 @@ namespace GeekShopping.Web.Controllers {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Login() {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Logout() {
+            return SignOut("Cookies", "oidc");
         }
     }
 }
