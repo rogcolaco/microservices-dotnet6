663ce4799895b8f5ae927ec6d6b3a3091251fd1b
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-25 18:40:20 -0300

    testes aplicar cupom

---
+++ geekShopping/GeekShopping/GeekShopping.Web/Services/IServices/ICouponService.cs
@@ -0,0 +1,7 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices {
+    public interface ICouponService {
+        Task<CouponViewModel> GetCoupon(string code, string token);
+    }
+}
