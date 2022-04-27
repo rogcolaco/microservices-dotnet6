3975fcc5958dd83f50129e2567a60dd45a0b6a25
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-04-27 11:58:23 -0300

    criadas migrations para DB da orderAPI

---
+++ geekShopping/GeekShopping/GeekShopping.OrderAPI/Program.cs
@@ -0,0 +1,88 @@
+using GeekShopping.OrderAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Configuração conexão banco de dados
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection,
+        new MySqlServerVersion(new Version(8, 0, 28))));
+
+//Configuracao repositorios
+//builder.Services.AddScoped<ICartRepository, CartRepository>();
+
+builder.Services.AddControllers();
+
+//Configuração authentication
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options => {
+        options.Authority = "https://localhost:44335/";
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateAudience = false
+        };
+    });
+
+//Configuração autorizacao
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("ApiScope", policy => {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.COrderAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Indicação da aplicação para utilização de HTTPS
+app.UseHttpsRedirection();
+
+//Indicação da aplicação para utilização de authentications
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
